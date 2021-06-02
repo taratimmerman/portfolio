1d47e4df2a08386430227209e3a1cda3e9b72d84
taratimmerman <timmermantara@gmail.com>
2021-06-01 18:10:05 -0600

    Build and style Home page

--- tara-portfolio/src/App.js
+++ tara-portfolio/src/App.js
@@ -1,21 +1,32 @@
+import { React, useState } from 'react';
 import './App.css';
+import Sidebar from './components/Sidebar';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import About from './components/About';
 import Work from './components/Work';
+import Home from './components/Home';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
+import styled from 'styled-components';
 
 function App() {
+  const [ isOpen, setIsOpen ] = useState(false)
+
+  const toggle = () => {
+      setIsOpen(!isOpen)
+  }
+
   return (
     <Router>
 
       <>
 
-        <Navbar />
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Navbar toggle={toggle} />
 
         <Switch>
           <Route path="/about">
@@ -23,13 +34,15 @@ function App() {
           </Route>
 
           <Route path="/work">
-            <Work />
+            <WorkWrapper>
+              <StyledH3>Work</StyledH3>
+              <Work />
+            </WorkWrapper>
           </Route>
 
           <Route path="/">
-
+            <Home />
           </Route>
-
         </Switch>
 
         <Footer />
@@ -41,3 +54,17 @@ function App() {
 }
 
 export default App;
+
+// STYLED COMPONENTS BELOW:
+
+const WorkWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+`;
+
+const StyledH3 = styled.h3`
+    margin: 5rem 0;
+    color: #404040;
+`;
